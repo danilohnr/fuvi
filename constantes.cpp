c018d533199595fde3508621fb87cc4a6c9a2b83
Danilo <danilo.noguera@gmail.com>
2022-10-04 09:37:13 -0600

    Se agregó de nuevo el archivo constantes.cpp

---
+++ constantes.cpp
@@ -0,0 +1,14 @@
+/*
+Nombre del archivo: constantes.cpp
+Autor: Danilo Noguera
+Lugar: ITV
+Instrucciones: Esto solo es una plantilla
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
