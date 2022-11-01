1b7d8ccd369a3e3617872f51969ea5d59df67715
Danilo <danilo.noguera@gmail.com>
2022-11-01 10:54:54 -0600

    Mas ejemplos con funciones

---
+++ ascii.cpp
@@ -0,0 +1,18 @@
+/*
+Nombre del archivo: sumanumeros.cpp
+Autor: Danilo Noguera
+Lugar: ITV
+Instrucciones: Esto solo es una plantilla
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	char caracter = 'R';
+	
+	cout << +caracter;
+	
+	return 0;
+}
